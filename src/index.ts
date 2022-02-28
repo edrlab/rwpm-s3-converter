a692cb2013f51639361dc7c49909a4ece8929ee6
Pierre Leroux <panacpp@gmail.com>
2022-02-28 15:19:47 +0100

    init cloud function

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,40 @@
-console.log('Try npm run lint/fix!');
+import {Request, Response} from '@google-cloud/functions-framework';
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const fn = async (req: Request, res: Response) => {
+  switch (req.method) {
+    case 'GET':
+      res.status(404).json({status: 'error', message: 'not implemented yet'});
+      break;
 
-const trailing = 'Semicolon';
-
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
+    case 'OPTIONS':
+      // Send response to OPTIONS requests
+      res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+      res.set('Access-Control-Allow-Headers', 'Content-Type');
+      res.set('Access-Control-Max-Age', '3600');
+      res.status(204).json({});
+      break;
+    default:
+      res.status(405).json({status: 'error', message: 'undefined method'});
+      break;
   }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
-}
-// TODO: more examples
+};
+
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+exports.manifest = async (req: Request, res: Response) => {
+  res.set('Access-Control-Allow-Origin', '*');
+
+  try {
+    return await fn(req, res);
+  } catch (e) {
+    console.log(e);
+
+    res.status(500).json({status: 'error', message: `${e}`});
+  }
+};
